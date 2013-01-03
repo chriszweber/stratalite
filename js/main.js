96f87f8ba2c968abc73cfede0edb4b79832b2964
Chris Zweber <chris.zweber@coracove.com>
2013-01-03 12:43:37 -0600

    changed website template

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$('.logo').hover(function(e){
-	if( $('a[href="/"]').hasClass('cur') ){ $('a[href="/"]').toggleClass('hover'); }
-	else { $('a[href="/"]').toggleClass('hover'); }
-})
-
-var pathArray = String(window.location.pathname);
-pathArray.split('/');
-$('a[href="'+pathArray+'"]').addClass('cur');
